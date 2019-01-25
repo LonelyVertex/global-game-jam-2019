73b2e608989c5b372888e63433f7c2aba8c20925
Jakub Mirovský <mirovsky.jakub@gmail.com>
2019-01-25 23:35:47 +0100

    add handle interaction script with overall interaction script

---
+++ Assets/Scripts/Interactions/Interaction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public abstract class Interaction : MonoBehaviour
+{
+    protected abstract string InteractionTag { get; }
+
+    bool interactionEnabled = false;
+    Collider currentCollider;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(InteractionTag)) return;
+
+        currentCollider = other;
+
+        ShowInteractionPopup();
+        interactionEnabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag(InteractionTag)) return;
+
+        currentCollider = null;
+
+        ShowInteractionPopup();
+        interactionEnabled = false;
+    }
+
+    protected void ShowInteractionPopup()
+    {
+        Debug.Log("Show interaction popup!");
+    }
+
+    void Update()
+    {
+        if (!interactionEnabled) return;
+
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))
+        {
+            Interact(currentCollider);
+        }
+    }
+
+    protected abstract void Interact(Collider collider);
+}
