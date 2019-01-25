686fcd9e8f2dcf9978bae9fbb2d4cf612d7d760a
Jan Slifka <jan.slifka@gmail.com>
2019-01-26 00:19:12 +0100

    Add door prefab

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,19 +5,35 @@ public class Door : MonoBehaviour
     [SerializeField]
     bool open;
 
+    [SerializeField]
+    float openingSpeed;
+
+    float currentRotation;
+    float desiredRotation;
+    float t;
+
     public void ToggleOpen()
     {
         open = !open;
-        StateChanged();
+        t = 0;
+        desiredRotation = open ? 90 : 0;
     }
 
-    void StateChanged()
+    void Update()
     {
-        // TODO do something to change visual state and collider
+        if (Mathf.Approximately(currentRotation, desiredRotation)) {
+            currentRotation = desiredRotation;
+        } else {
+            currentRotation = Mathf.Lerp(currentRotation, desiredRotation, t);
+            t += openingSpeed * Time.deltaTime;
+        }
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, currentRotation, 0));
     }
 
     void OnValidate()
     {
-        StateChanged();
+        var rotation = open ? 90 : 0;
+        transform.rotation = Quaternion.Euler(new Vector3(0, rotation, 0));
     }
 }
