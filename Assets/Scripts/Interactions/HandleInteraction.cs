1e315dd31eb785b30893141816d4149e7866bff0
Jan Slifka <jan.slifka@gmail.com>
2019-01-26 00:27:29 +0100

    Add interactions to the test level

--- Assets/Scripts/Interactions/HandleInteraction.cs
+++ Assets/Scripts/Interactions/HandleInteraction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HandleInteraction : Interaction
 {
-    protected override string InteractionTag { get { return "Handles";  } }
+    protected override string InteractionTag => "Handle";
 
     protected override void Interact(Collider collider)
     {
