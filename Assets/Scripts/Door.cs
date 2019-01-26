521fbdef9e6eccb3d7ef3cd252e8b0a225d43bb5
Jan Slifka <jan.slifka@gmail.com>
2019-01-26 10:04:15 +0100

    Fix door and player's light

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,22 +1,41 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField]
+    Transform doorWrapper;
+
     [SerializeField]
     bool open;
 
     [SerializeField]
     float openingSpeed;
 
+    [SerializeField]
+    float openRotation = 90;
+
     float currentRotation;
     float desiredRotation;
     float t;
+
+    void Start()
+    {
+        SetDesiredRotation();
+    }
+
+    void SetDesiredRotation()
+    {
+        desiredRotation = open ? openRotation : 0;
+    }
 
     public void ToggleOpen()
     {
         open = !open;
         t = 0;
-        desiredRotation = open ? 90 : 0;
+        SetDesiredRotation();
     }
 
     void Update()
@@ -28,12 +47,22 @@ public class Door : MonoBehaviour
             t += openingSpeed * Time.deltaTime;
         }
 
-        transform.rotation = Quaternion.Euler(new Vector3(0, currentRotation, 0));
+        doorWrapper.localRotation = Quaternion.Euler(new Vector3(0, currentRotation, 0));
     }
 
     void OnValidate()
     {
-        var rotation = open ? 90 : 0;
-        transform.rotation = Quaternion.Euler(new Vector3(0, rotation, 0));
+        if (doorWrapper != null) {
+            var rotation = open ? openRotation : 0;
+            doorWrapper.localRotation = Quaternion.Euler(new Vector3(0, rotation, 0));
+        }
     }
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        Handles.color = new Color(1f, 0.0f, 1f, 0.3f);
+        Handles.DrawSolidArc(transform.position, transform.up, transform.right, openRotation, 1);
+    }
+#endif
 }
