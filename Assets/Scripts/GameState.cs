59c52d7f6c3b131f00b8c7650b87fbbe44f9b0b8
Jakub Mirovský <mirovsky.jakub@gmail.com>
2019-01-26 14:05:58 +0100

    add scene switching to gamestate

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,13 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class GameState : MonoBehaviour
 {
+    const string CURRENT_SCENE = "CURRENT_SCENE";
+
     public static GameState instance = null;
+
+    public SceneField nextScene;
+    public float sceneSwitchDelay = 1.5f;
 
     public bool IsPlaying { get; private set; } = true;
     public bool IsShowingUI { get; private set; } = false;
-
 
     void Awake()
     {
@@ -40,5 +46,24 @@ public class GameState : MonoBehaviour
     {
         IsShowingUI = false;
         IsPlaying = true;
+    }
+
+    public void NextScene()
+    {
+        StartCoroutine(NextSceneDelayed());
+    }
+
+    IEnumerator NextSceneDelayed()
+    {
+        var sceneLoading = SceneManager.LoadSceneAsync(nextScene);
+        sceneLoading.allowSceneActivation = false;
+
+        var progress = 0f;
+        while (sceneLoading.progress < 0.9f || progress < sceneSwitchDelay) {
+            progress += Time.deltaTime;
+            yield return null;
+        }
+
+        sceneLoading.allowSceneActivation = true;
     }
 }
