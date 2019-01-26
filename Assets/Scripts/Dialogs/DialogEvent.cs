3ec013d7119b34981829eedbdefa33f81019dbd0
Jakub Mirovský <mirovsky.jakub@gmail.com>
2019-01-26 10:22:20 +0100

    add dialog scriptable objects

---
+++ Assets/Scripts/Dialogs/DialogEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "Dialogs/Dialog Event")]
+public class DialogEvent : ScriptableObject
+{
+    public string title;
+
+    [Header("Content")]
+    public Image image;
+    public string[] texts;
+}
