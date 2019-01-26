f3f62f31eccd9de45ff07b32da5098207a6bb29a
Jakub Mirovský <mirovsky.jakub@gmail.com>
2019-01-26 18:51:59 +0100

    fix intro scene

---
+++ Assets/Scripts/Menu/SceneStartTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SceneSwitcher))]
+[RequireComponent(typeof(DialogController))]
+public class SceneStartTrigger : MonoBehaviour
+{
+    [SerializeField]
+    float sceneSwitchDelay;
+
+    SceneSwitcher sceneSwitcher;
+    DialogController controller;
+
+
+    void Start()
+    {
+        controller = GetComponent<DialogController>();
+        sceneSwitcher = GetComponent<SceneSwitcher>();
+
+        controller.StartDialog();
+
+        StartCoroutine(SwitchScene());
+    }
+
+    IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(sceneSwitchDelay);
+
+        sceneSwitcher.Switch();
+    }
+}
