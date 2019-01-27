61516c56778ac57eaa6d8f2130cbec143740cd96
Jakub Mirovský <mirovsky.jakub@gmail.com>
2019-01-27 11:26:46 +0100

    add ingame ui to level1, level2, uncle office level

---
+++ Assets/Scripts/Menu/InGameUIController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+public class InGameUIController : MonoBehaviour
+{
+    public SceneField menuScene;
+    public GameObject uiObject;
+
+    GameState gameState;
+
+
+    public void ToMenu()
+    {
+        gameState.nextScene = menuScene;
+        gameState.NextScene();
+    }
+
+    public void Continue()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        uiObject.SetActive(false);
+        gameState = GameState.instance;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            uiObject.SetActive(!uiObject.activeSelf);
+        }
+    }
+}
